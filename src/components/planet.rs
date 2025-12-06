ec3fe22125a78ab497840a696c7da4f4c855c1d5
tommasoasko <tommaso.ascolani04@gmail.com>
2025-12-06 11:52:54 +0100

    Correctly implemented handle_asteroid and implemented new logic to search energy cells

--- src/components/planet.rs
+++ src/components/planet.rs
@@ -1,6 +1,5 @@
 use std::collections::{HashMap, HashSet};
 use std::sync::mpsc;
-use common_game::components::asteroid::Asteroid;
 // use std::time::SystemTime;
 use common_game::components::planet::{Planet, PlanetAI, PlanetState, PlanetType};
 use common_game::components::resource::BasicResourceType::Carbon;
@@ -10,6 +9,8 @@ use common_game::components::rocket::Rocket;
 use common_game::protocols::messages::{
     ExplorerToPlanet, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator,
 };
+use common_game::protocols::messages::OrchestratorToPlanet::Asteroid;
+use crate::components::planet::stacks::{CHARGED_CELL_STACK, FREE_CELL_STACK};
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // CrabRave Constructor
@@ -56,6 +57,8 @@ pub struct AI;
 
 impl AI {
     fn new() -> Self {
+        // TODO non so se va qui o nello start AI
+        initialize_free_cell_stack(); // TODO rimuovere se si sceglie la vecchia implementazione
         Self
     }
 }
@@ -77,15 +80,23 @@ impl PlanetAI for AI {
                 })
             }
             OrchestratorToPlanet::Sunray(sunray) => {
-                for i in 0..N_CELLS {
-                    // non ho trovato un modo per ottenere il vettore, l'unico modo penso sia quello di ciclare
-                    if !state.cell(i).is_charged() {
-                        state.cell_mut(i).charge(sunray);
-                        return Some(PlanetToOrchestrator::SunrayAck {
-                            planet_id: state.id(),
-                            // timestamp: SystemTime::now(),
-                        });
-                    }
+                // for i in 0..N_CELLS {
+                //     // non ho trovato un modo per ottenere il vettore, l'unico modo penso sia quello di ciclare
+                //     if !state.cell(i).is_charged() {
+                //         state.cell_mut(i).charge(sunray);
+                //         return Some(PlanetToOrchestrator::SunrayAck {
+                //             planet_id: state.id(),
+                //             // timestamp: SystemTime::now(),
+                //         });
+                //     }
+                // }
+                // None
+                if let Some(idx) = get_free_cell_index() {
+                    state.cell_mut(idx as usize).charge(sunray);
+                    push_charged_cell(idx);
+                    return Some(PlanetToOrchestrator::SunrayAck {
+                        planet_id: state.id().clone(),
+                    })
                 }
                 None
             }
@@ -127,12 +138,19 @@ impl PlanetAI for AI {
         match msg {
             ExplorerToPlanet::AvailableEnergyCellRequest { explorer_id: _ } => {
                 // restituisce la prima cell carica, se c'è
-                for i in 0..N_CELLS {
-                    if state.cell(i).is_charged() {
-                        return Some(PlanetToExplorer::AvailableEnergyCellResponse {
-                            available_cells: i as u32,
-                        });
-                    }
+                // DO NOT REMOVE -> the following commented lines are the old implementation, so do not remove them till the final decision of the implementation
+                // for i in 0..N_CELLS {
+                //     if state.cell(i).is_charged() {
+                //         return Some(PlanetToExplorer::AvailableEnergyCellResponse {
+                //             available_cells: i as u32,
+                //         });
+                //     }
+                // }
+                // None
+                if let Some(idx) = peek_charged_cell_index() {
+                    return Some(PlanetToExplorer::AvailableEnergyCellResponse {
+                        available_cells: idx
+                    })
                 }
                 None
             }
@@ -154,11 +172,13 @@ impl PlanetAI for AI {
             } => {
                 let requested_resource = resource;
                 // controllo se c'è una cella carica
-                let cell_idx = (0..N_CELLS).find(|&i| state.cell(i).is_charged());
-                if let Some(cell_idx) = cell_idx {
+                // DO NOT REMOVE -> the following commented lines are the old implementation, so do not remove them till the final decision of the implementation
+                // let cell_idx = (0..N_CELLS).find(|&i| state.cell(i).is_charged());
+                // if let Some(cell_idx) = cell_idx {
+                if let Some(cell_idx) = get_charged_cell_index() {
                     // se c'è una cella carica
                     // ottengo la cella da passare al generator
-                    let cell = state.cell_mut(cell_idx);
+                    let cell = state.cell_mut(cell_idx as usize); // TODO remove the "as usize" if using the old implementation of getting the index of energy cell
                     // pattern matching per generare la risorsa corretta
                     let generated_resource = match requested_resource {
                         BasicResourceType::Carbon => {
@@ -177,16 +197,17 @@ impl PlanetAI for AI {
                     // verifico il risultato di state.generator.make...
                     match generated_resource {
                         Ok(resource) => {
+                            push_free_cell(cell_idx);
                             return Some(PlanetToExplorer::GenerateResourceResponse {
                                 resource: Some(resource),
                             });
                         }
                         Err(err) => {
+                            push_charged_cell(cell_idx);
                             println!("{}", err);
                         }
                     }
-                }
-                if cell_idx.is_none() {
+                } else {
                     println!("No available cell found"); // non dovrebbe accadere, si spera che l'explorer chieda se ce ne è una libera
                 }
                 Some(PlanetToExplorer::GenerateResourceResponse {
@@ -199,9 +220,11 @@ impl PlanetAI for AI {
             //TODO use explorer_id to send the gen resource to correct Explorer
             ExplorerToPlanet::CombineResourceRequest { explorer_id, msg } => {
                 // searching the index of the first free cell
-                let cell_idx = (0..N_CELLS).find(|&i| state.cell(i).is_charged());
-                if let Some(cell_idx) = cell_idx {
-                    let cell = state.cell_mut(cell_idx);
+                // DO NOT REMOVE -> the following commented lines are the old implementation, so do not remove them till the final decision of the implementation
+                // let cell_idx = (0..N_CELLS).find(|&i| state.cell(i).is_charged());
+                // if let Some(cell_idx) = cell_idx {
+                if let Some(cell_idx) = get_charged_cell_index() {
+                    let cell = state.cell_mut(cell_idx as usize); // TODO remove the "as usize" if using the old implementation of getting the index of energy cell
                     // pattern matching to generate the correct resource
                     let complex_resource: Result<ComplexResource, (String, GenericResource, GenericResource)> = match msg {
                         ComplexResourceRequest::Water(r1, r2) => combinator
@@ -236,11 +259,13 @@ impl PlanetAI for AI {
                     // checking the result of complex_resource
                     return match complex_resource {
                         Ok(resource) => {
+                            push_free_cell(cell_idx);
                             Some(PlanetToExplorer::CombineResourceResponse {
                                 complex_response: Ok(resource),
                             })
                         }
                         Err(err) => {
+                            push_charged_cell(cell_idx);
                             println!("{}", err.0);
                             Some(PlanetToExplorer::CombineResourceResponse {
                                 complex_response: Err(err),
@@ -263,13 +288,53 @@ impl PlanetAI for AI {
         generator: &Generator,
         combinator: &Combinator,
     ) -> Option<Rocket> {
-        state.take_rocket()
+        if !state.has_rocket(){ // TODO this is the case in which we do not have a planet with rockets, if so we can return None anyway (to be removed if our planet is of a rocket type)
+            return None;
+        }
+
+        // if there is no rocket at the moment, try to build one (if there is a charged energy cell available)
+
+        // try to take the rocket
+        let mut res = state.take_rocket();
+        if res.is_none() {
+
+            // try to find the charged energy cell
+            if let Some(idx) = get_charged_cell_index() {
+
+                // try to build the rocket
+                match state.build_rocket(idx as usize) {
+                    Ok(_) => {
+
+                        // discharging the cell used to build the rocket
+                        push_free_cell(idx);
+                        match state.cell_mut(idx as usize).discharge() {
+                            Ok(_) => {
+                                println!("Used a charged cell at index {}, to build a rocket", idx);
+                            }
+                            Err(err) => {
+                                println!("{}", err);
+                            }
+                        }
+
+                        // taking the new rocket
+                        res = state.take_rocket();
+                    }
+                    Err(err) => {
+                        push_free_cell(idx);
+                        println!("{}", err);
+                    }
+                }
+            }
+        }
+        res
     }
 
     fn start(&mut self, state: &PlanetState) {
         println!("Planet {} AI started", state.id());
         // TODO non ho capito bene cosa deve fare planet.ai.start, deve creare il thread o lo fa l'orchestrator?
         // Mi sembra che lo start AI semplicemente dia il via al loop che permette l'AI di gestire le azioni
+        // TODO non so se ha senso mettere l'inizializzazione degli stack qui o se va messa quando creaiamo AI
+        // initialize_free_cell_stack() // TODO remove if the choice is the old implementation
     }
 
     fn stop(&mut self, _state: &PlanetState) {
@@ -277,3 +342,42 @@ impl PlanetAI for AI {
         // TODO stessa cosa di "start"
     }
 }
+
+mod stacks {
+    use std::sync::Mutex;
+    pub(super) static FREE_CELL_STACK: Mutex<Vec<u32>> = Mutex::new(Vec::new());
+    pub(super) static CHARGED_CELL_STACK: Mutex<Vec<u32>> = Mutex::new(Vec::new());
+}
+
+fn initialize_free_cell_stack(){
+    let mut free_cell_stack = FREE_CELL_STACK.lock().unwrap();
+    for i in 0..N_CELLS {
+        free_cell_stack.push(i as u32);
+    }
+}
+
+fn get_free_cell_index() -> Option<u32> {
+    let mut free_cell_stack = FREE_CELL_STACK.lock().unwrap();
+    free_cell_stack.pop()
+}
+
+fn get_charged_cell_index() -> Option<u32> {
+    let mut charged_cell_stack = CHARGED_CELL_STACK.lock().unwrap();
+    charged_cell_stack.pop()
+}
+
+fn push_free_cell(index: u32) {
+    let mut free_cell_stack = FREE_CELL_STACK.lock().unwrap();
+    free_cell_stack.push(index);
+}
+
+fn push_charged_cell(index: u32) {
+    let mut charged_cell_stack = CHARGED_CELL_STACK.lock().unwrap();
+    charged_cell_stack.push(index);
+}
+
+fn peek_charged_cell_index() -> Option<u32> {
+    let charged_cell_stack = CHARGED_CELL_STACK.lock().unwrap();
+    charged_cell_stack.last().copied()
+}
+
