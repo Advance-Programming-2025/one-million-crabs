4b6c0b7af5de78d752e8a44889e9bfd2c94dfd76
tommasoasko <tommaso.ascolani04@gmail.com>
2025-12-08 22:17:07 +0100

    Added t10

--- src/components/tests.rs
+++ src/components/tests.rs
@@ -5,7 +5,8 @@ use common_game::components::sunray::Sunray;
 use common_game::protocols::messages::{ExplorerToOrchestrator, ExplorerToPlanet, OrchestratorToExplorer, OrchestratorToPlanet, PlanetToExplorer, PlanetToOrchestrator};
 use crossbeam_channel::{unbounded, Receiver, RecvError, Sender};
 use std::thread;
-use common_game::components::resource::BasicResourceType;
+use common_game::components::resource::{BasicResource, BasicResourceType, Carbon, ComplexResource, ComplexResourceRequest, GenericResource};
+use common_game::components::resource::GenericResource::BasicResources;
 use common_game::components::rocket::Rocket;
 use crate::components::energy_stacks::N_CELLS;
 use crate::components::energy_stacks::stacks::{get_free_cell_index, push_free_cell};
@@ -317,7 +318,6 @@ fn t03_correct_resource_request() -> Result<(), String> {
         Ok(Err(e)) => Err(format!("Planet thread returned error: {}", e)),
         Err(_) => Err("Planet thread panicked".to_string()),
     }
-
 }
 
 #[test]
@@ -354,7 +354,7 @@ fn t04_failure_resource_request() -> Result<(), String> {
         Err(err)=>{ panic!("Failed to start planet AI: {}.", err); }
     }
 
-    register_the_explorer(&orchestrator);
+    register_the_explorer(&orchestrator)?;
 
     match &orchestrator.explorers[0] {
         Explorer { planet_id, orchestrator_channels, explorer_to_planet_channels, planet_to_explorer_channels } => {
@@ -756,6 +756,147 @@ fn t09_combination_rules_request()->Result<(),String> {
                         }
                         Ok(_) => { panic!("Unexpected combination rules request response."); }
                         Err(err)=>{ panic!("Failed to respond to combination rules request: {}", err); }
+                    }
+                }
+            }
+        }
+    }
+
+    let result = killing_planet(&orchestrator);
+
+    match handle.join() {
+        Ok(Ok(_)) => {
+            println!("Planet thread completed successfully");
+            result
+        }
+        Ok(Err(e)) => Err(format!("Planet thread returned error: {}", e)),
+        Err(_) => Err("Planet thread panicked".to_string()),
+    }
+}
+
+#[test]
+fn  t10_combination_request()->Result<(),String> {
+    println!("+++++ Correct resource request +++++");
+    let mut orchestrator = Orchestrator::new()?;
+    let mut planet1 = match orchestrator.galaxy_topology.pop(){
+        Some(p)=>p,
+        None=>return Err("Cannot find any planet to pop".to_string()),
+    };
+
+    println!("Creating planet thread...");
+    let handle = thread::spawn(move ||->Result<(), String>{
+        println!("Planet running...");
+        planet1.run()
+    });
+
+    println!("Start Planet...");
+    match orchestrator.planet_channels.1.send(OrchestratorToPlanet::StartPlanetAI) {
+        Ok(_) => { println!("Planet AI started."); },
+        Err(err)=>{ panic!("Failed to start planet AI: {}", err); },
+    }
+
+    println!("Waiting for response...");
+    match orchestrator.planet_channels.0.recv(){
+        Ok(res) => {
+            match res {
+                PlanetToOrchestrator::StartPlanetAIResult { planet_id } => {
+                    println!("Planet {} AI started.", planet_id);
+                },
+                _ => panic!("Unexpected response to StartPlanetAI.")
+            }
+        }
+        Err(err)=>{ panic!("Failed to start planet AI: {}.", err); }
+    }
+
+    sending_sunray(&orchestrator)?;
+    sending_sunray(&orchestrator)?;
+    sending_sunray(&orchestrator)?;
+
+    register_the_explorer(&orchestrator)?;
+
+    let mut resource1 = None;
+    let mut resource2 = None;
+
+    match &orchestrator.explorers[0] {
+        Explorer { planet_id, orchestrator_channels, explorer_to_planet_channels, planet_to_explorer_channels } => {
+
+            println!("Accessing planet-explorer channels...");
+            match explorer_to_planet_channels {
+                None => { panic!("Planet channels is None."); }
+                Some(channels) => {
+
+                    println!("Sending resource request...");
+                    match channels.1.send(ExplorerToPlanet::GenerateResourceRequest { explorer_id: 0, resource: BasicResourceType::Carbon}) {
+                        Ok(_) => { println!("Planet generated resource request."); },
+                        Err(err)=>{ panic!("Failed to generate resource request: {}", err); }
+                    }
+
+                    println!("Responding to resource request...");
+                    match channels.0.recv() {
+                        Ok(PlanetToExplorer::GenerateResourceResponse { resource }) => {
+                            if resource.is_some() {
+                                println!("Explorer received the resource request response correctly.");
+                                resource1 = Some(resource.unwrap());
+                            } else {
+                                println!("Planet gave back None resource.");
+                            }
+                        }
+                        Ok(_) => { panic!("Unexpected resource request response."); }
+                        Err(err)=>{ panic!("Failed to respond to resource request: {}", err); }
+                    }
+                }
+            }
+
+            println!("Accessing planet-explorer channels...");
+            match explorer_to_planet_channels {
+                None => { panic!("Planet channels is None."); }
+                Some(channels) => {
+
+                    println!("Sending resource request...");
+                    match channels.1.send(ExplorerToPlanet::GenerateResourceRequest { explorer_id: 0, resource: BasicResourceType::Carbon}) {
+                        Ok(_) => { println!("Planet generated resource request."); },
+                        Err(err)=>{ panic!("Failed to generate resource request: {}", err); }
+                    }
+
+                    println!("Responding to resource request...");
+                    match channels.0.recv() {
+                        Ok(PlanetToExplorer::GenerateResourceResponse { resource }) => {
+                            if resource.is_some() {
+                                println!("Explorer received the resource request response correctly.");
+                                resource2 = Some(resource.unwrap());
+                            } else {
+                                println!("Planet gave back None resource.");
+                            }
+                        }
+                        Ok(_) => { panic!("Unexpected resource request response."); }
+                        Err(err)=>{ panic!("Failed to respond to resource request: {}", err); }
+                    }
+                }
+            }
+
+            println!("Accessing planet-explorer channels...");
+            match explorer_to_planet_channels {
+                None => { panic!("Planet channels is None."); }
+                Some(channels) => {
+
+
+                    println!("Sending combination request...");
+                    match channels.1.send(ExplorerToPlanet::CombineResourceRequest { explorer_id: 0, msg: ComplexResourceRequest::Diamond(resource1.unwrap().to_carbon()?, resource2.unwrap().to_carbon()?)}) {
+                        Ok(_) => { println!("Planet generated combination request."); },
+                        Err(err)=>{ panic!("Failed to generate combination request: {}", err); }
+                    }
+
+                    println!("Responding to resource request...");
+                    match channels.0.recv() {
+                        Ok(PlanetToExplorer::CombineResourceResponse { complex_response }) => {
+                            if complex_response.is_ok() {
+                                println!("Explorer received the resource request response correctly.");
+                            } else {
+                                println!("Planet gave back None resource.");
+                            }
+                        }
+                        Ok(_) => { panic!("Unexpected resource request response."); }
+                        Err(err)=>{ panic!("Failed to respond to resource request: {}", err); }
                     }
                 }
             }
